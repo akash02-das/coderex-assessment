45799cdb482fef497866c3d14306d9ad821b8243
Akash Das <aku.das02@gmail.com>
2023-04-01 22:43:38 +0600

    add error page for invalid routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 
 import "./App.css";
 import Nav from "./components/Navbar/Nav";
+import Error from "./pages/Error";
 import Home from "./pages/Home";
 import Users from "./pages/Users";
 
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/users" element={<Users />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </div>
   );
---
+++ src/pages/Error.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Error = () => {
+  return (
+    <div className="w-full h-screen flex items-center justify-center flex-col">
+      <h1 className="text-3xl block text-red-700">Error! 404</h1>
+      <h3 className="text-2xl">Page Not Found</h3>
+    </div>
+  );
+};
+
+export default Error;
